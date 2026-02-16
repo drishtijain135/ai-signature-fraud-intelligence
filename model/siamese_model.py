0e850c232aa231dd886a35c37b9608478ce83f10
Prachi Rajput <prachiraj9149@gmail.com>
2026-02-17 01:51:20 +0530

    Finalized Siamese model architecture

--- model/siamese_model.py
+++ model/siamese_model.py
@@ -1,38 +1,64 @@
 import tensorflow as tf
 from tensorflow.keras import layers, Model
 
-IMG_WIDTH = 220
 IMG_HEIGHT = 155
+IMG_WIDTH = 220
+
 
 def build_base_network():
-    input = layers.Input((IMG_HEIGHT, IMG_WIDTH, 1))
+    """
+    CNN feature extractor
+    """
 
-    x = layers.Conv2D(32, (3,3), activation='relu')(input)
+    inputs = tf.keras.Input(shape=(IMG_HEIGHT, IMG_WIDTH, 1))
+
+    x = layers.Conv2D(32, (3, 3), activation="relu")(inputs)
+    x = layers.BatchNormalization()(x)
     x = layers.MaxPooling2D()(x)
 
-    x = layers.Conv2D(64, (3,3), activation='relu')(x)
+    x = layers.Conv2D(64, (3, 3), activation="relu")(x)
+    x = layers.BatchNormalization()(x)
     x = layers.MaxPooling2D()(x)
 
-    x = layers.Conv2D(128, (3,3), activation='relu')(x)
+    x = layers.Conv2D(128, (3, 3), activation="relu")(x)
+    x = layers.BatchNormalization()(x)
     x = layers.MaxPooling2D()(x)
 
     x = layers.Flatten()(x)
-    x = layers.Dense(128, activation='relu')(x)
-    x = layers.Dropout(0.4)(x)
+    x = layers.Dense(256, activation="relu")(x)
+    x = layers.Dropout(0.3)(x)
 
-    return Model(input, x)
+    outputs = layers.Dense(128)(x)
+
+    return Model(inputs, outputs, name="FeatureExtractor")
+
 
 def build_siamese_model():
+    """
+    Siamese Network using distance metric
+    """
+
     base_network = build_base_network()
 
-    input_a = layers.Input((IMG_HEIGHT, IMG_WIDTH, 1))
-    input_b = layers.Input((IMG_HEIGHT, IMG_WIDTH, 1))
+    input_a = tf.keras.Input(shape=(IMG_HEIGHT, IMG_WIDTH, 1))
+    input_b = tf.keras.Input(shape=(IMG_HEIGHT, IMG_WIDTH, 1))
 
-    processed_a = base_network(input_a)
-    processed_b = base_network(input_b)
+    embedding_a = base_network(input_a)
+    embedding_b = base_network(input_b)
 
-    merged = layers.Concatenate()([processed_a, processed_b])
-    output = layers.Dense(1, activation='sigmoid')(merged)
+    # Euclidean distance
+    distance = tf.keras.layers.Lambda(
+        lambda tensors: tf.math.abs(tensors[0] - tensors[1])
+    )([embedding_a, embedding_b])
 
-    model = Model([input_a, input_b], output)
+    output = layers.Dense(1, activation="sigmoid")(distance)
+
+    model = Model(inputs=[input_a, input_b], outputs=output)
+
+    model.compile(
+        loss="binary_crossentropy",
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0005),
+        metrics=["accuracy"]
+    )
+
     return model
