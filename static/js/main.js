1f1247cc7288ec8e46827075235567089a387efa
dangibharti1511 <dangibharti15@gmail.com>
2026-02-18 18:13:41 +0530

    UI enhancement: premium glass effect, animated navbar, button redesign, improved layout

--- static/js/main.js
+++ static/js/main.js
@@ -76,3 +76,13 @@ function launchConfetti() {
         }, 4000);
     }
 }
+window.addEventListener("scroll", function () {
+    const nav = document.querySelector("nav");
+
+    if (window.scrollY > 20) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
+
